e7c4db5796810339103fef7ddb4fbe3597124103
yudppp <yu.d.ppp@gmail.com>
2017-02-27 23:02:04 +0900

    Add exec options

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 var pkg = require('./package');
 var emoji = require('node-emoji')
+var inquirer = require('inquirer')
 
 module.exports = function (p, exec, log, cwd, options) {
 	function printHelp() {
@@ -28,10 +29,30 @@ module.exports = function (p, exec, log, cwd, options) {
 		})
 	}
 
+	function execList() {
+		log.info(emoji.get('shell') + ' tsks');
+		var tasks = require('./tasks').getTasks(exec, cwd)
+		inquirer.prompt([{
+			type: 'list',
+			name: 'task',
+			message: 'Which do you run task',
+			choices: tasks
+		}]).then(function(answers){
+			console.log(`exec: ${answers.task}`)
+			var cmd = `cd ${cwd}; ${answers.task}`;
+			require('child_process').exec(cmd, function(error, stdout, stderr) {
+				console.log(stdout)
+				console.log(stderr)
+			});
+		})
+	}
+
 	if (options.help) {
 		printHelp();
 	} else if (options.version) {
 		printVersion();
+	} else if (options.exec){
+		return execList();
 	} else {
 		return showList();
 	}
